3912e9df8f192b8d1414852a5400b5df40a28841
Monali <monalimirel@gmail.com>
2018-09-20 13:20:37 -0700

    init push

---
+++ SpyApp/SpyApp/CeaserCipher.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+protocol Cipher {
+    func encode(_ plaintext: String, secret: String) -> String?
+    //func decrypt(_ encryptedtext: String, secret: String) -> String?
+}
+
+struct CeaserCipher: Cipher {
+
+    func encode(_ plaintext: String, secret: String) -> String? {
+        guard let shiftBy = UInt32(secret) else {
+            return nil
+        }
+        var encoded = ""
+
+        for character in plaintext {
+            let unicode = character.unicodeScalars.first!.value
+            let shiftedUnicode = unicode + shiftBy
+            let shiftedCharacter = String(UnicodeScalar(UInt8(shiftedUnicode)))
+            encoded = encoded + shiftedCharacter
+        }
+        return encoded
+    }
+    
+//    func decrypt(_ encryptedtext: String, secret: String) -> String? {
+//        guard let shiftBy = UInt32(secret) else {
+//            return nil
+//        }
+//
+//        var decrypted = ""
+//
+//        for character in encryptedtext {
+//            let unicode = character.unicodeScalars.first!.value
+//            let shiftedUnicode = unicode + shiftBy
+//            let shiftedCharacter = String(UnicodeScalar(UInt8(shiftedUnicode)))
+//            decrypted = decrypted - shiftedCharacter
+//        }
+//    }
+    
+}
